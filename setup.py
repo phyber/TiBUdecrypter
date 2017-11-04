c03f24e18a3fbb750d08c9753ff873c663cfd250
phyber <phyber@localhost>
2017-11-04 13:36:45 +0000

    Change pycrypto dep to pycryptodome

--- setup.py
+++ setup.py
@@ -72,7 +72,11 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['docopt', 'six', 'pycrypto'],
+    install_requires=[
+        'docopt',
+        'pycryptodome',
+        'six',
+    ],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
